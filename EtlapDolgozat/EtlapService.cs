c20cedc17a220383fd248c3e3bcaca798e0d46e6
Petrik Diak <diak@petrik.hu>
2023-12-14 10:10:41 +0100

    10:10 Cmon my Nigga

---
+++ EtlapDolgozat/EtlapService.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtlapDolgozat
+{
+	public class EtlapService
+	{
+		MySqlConnection connection;
+
+		public EtlapService()
+		{
+			MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+			builder.Server = "localhost";
+			builder.Port = 3306;
+			builder.UserID = "root";
+			builder.Password = "";
+			builder.Database = "etlapdb";
+
+			connection = new MySqlConnection(builder.ConnectionString);
+		}
+
+		public List<Etel> GetAll()
+		{
+			List<Etel> etlap = new List<Etel>();
+			OpenConnection();
+			string sql = "SELECT * FROM etlap";
+			MySqlCommand command = connection.CreateCommand();
+			command.CommandText = sql;
+			using (MySqlDataReader reader = command.ExecuteReader())
+			{
+				while (reader.Read())
+				{
+					Etel etel = new Etel();
+					etel.Id = reader.GetInt32("id");
+					etel.Nev = reader.GetString("nev");
+					etel.Leiras = reader.GetString("leiras");
+					etel.Ar = reader.GetInt32("ar");
+					etel.Kategoria = reader.GetString("kategoria");
+					etlap.Add(etel);
+				}
+			}
+			CloseConnection();
+			return etlap;
+		}
+
+		private void CloseConnection()
+		{
+			if (connection.State == System.Data.ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+
+		private void OpenConnection()
+		{
+			if (connection.State != System.Data.ConnectionState.Open)
+			{
+				connection.Open();
+			}
+		}
+	}
+}
--- EtlapDolgozat/MainWindow.xaml.cs
+++ EtlapDolgozat/MainWindow.xaml.cs
@@ -20,10 +20,27 @@ namespace EtlapDolgozat
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		EtlapService service = new EtlapService();
 		public MainWindow()
 		{
 			// Azért új projektet kellett létrehoznom emiatt EtlapDolgozat a neve és nem sima Etlap ez a gitrepóra is igaz
 			InitializeComponent();
+			Read();
+			
+		}
+
+		private void Read()
+		{
+			etlapTable.ItemsSource = service.GetAll();
+			
+		}
+
+
+		private void etlapTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Etel etel = etlapTable.SelectedItem as Etel;
+			etelLabel.Content = etel.Leiras;
+
 		}
 	}
 }
